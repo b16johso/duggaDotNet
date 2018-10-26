dd2314014377d4b5b1715d226c033834b70e1ce9
John <b16johso@student.his.se>
2018-10-26 03:08:15 +0200

    Add project files.

---
+++ duggaDotNet/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using duggaDotNet.Models;
+
+namespace duggaDotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            CustomersModel sm = new CustomersModel();
+            ViewBag.CustomerTable = sm.GetAllCustomers();
+            return View();
+        }
+    }
+}
---
+++ duggaDotNet/Models/CustomersModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace duggaDotNet.Models
+{
+    public class CustomersModel
+    {
+        private string connectionString = "Server=localhost; Port=3306; Database=b16johso; Uid=root; Pwd=; SslMode=none;";
+
+        public DataTable GetAllCustomers()
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM alien;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable customerTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return customerTable;
+        }
+    }
+}
